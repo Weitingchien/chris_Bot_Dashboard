5aa09d0748fb5204f04241fa33f7517362d8a4bd
Ting <chienweiting3839@gmail.com>
2022-06-21 19:32:36 +0800

    Modified main to __main__

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 
 from flask import Flask
+print(Flask)
+app = Flask(__name__)  # 用來定位目前載入資料夾的位置，判別template_folder或static_folder資料夾位置
 
-app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/")  # decorator是用來讓Flask監聽此URL，並返回結果
 def home_view():
     return "<h1>Welcome Dashboard</h1>"
